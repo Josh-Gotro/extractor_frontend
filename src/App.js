e102f4b26daf90b438df798702f45c7e4e49fc69
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-05-26 16:51:18 -0500

    file drop complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ImageUpload from './components/ImageUpload'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      < ImageUpload onFilesAdded={console.log("file added")}/>
-    </div>
-  );
-}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      allImages: null
+    }
+  }
+
+  doSomething = (something) => {
+    console.log(something)
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        < ImageUpload onFilesAdded={this.doSomething} />
+      </div>
+    );
+  }
+
+};
+
 
 export default App;
+
+

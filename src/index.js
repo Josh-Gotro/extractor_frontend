4509bae0bd7dc65fb2a281c2d59165a19f0151c4
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-05-29 11:12:53 -0500

    routes technically functional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+  BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 // import DisplayColors from './components/DisplayColors'
 

c58975c8f5dc4e77822829066985c2fad212108a
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-06-24 11:02:32 -0500

    update for showcase

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,10 @@ const About = () => (
 
 
 <div className="SomeText">
-There is an interaction and action, reaction between two people.
+{/* There is an interaction and action, reaction between two people. */}
+You can’t change the feeling<br></br>
+but you can change your feelings about the feeling in a second or two…<br></br>
+-david berman
 </div>
    
 )
